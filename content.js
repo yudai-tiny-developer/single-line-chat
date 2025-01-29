599c2a8c24f45ae9a78e42ede86c921b0d334ee5
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2025-01-29 17:16:39 +0900

    v1.6.1

--- content.js
+++ content.js
@@ -7,13 +7,13 @@ import(chrome.runtime.getURL('common.js')).then(common => {
 function main(common) {
     function loadSettings() {
         chrome.storage.local.get(common.storage, data => {
-            document.documentElement.style.setProperty('--single-line-chat-icon', common.value(data.hide_icon, common.default_hide_icon) ? 'none' : 'block');
-            document.documentElement.style.setProperty('--single-line-chat-name', common.value(data.hide_name, common.default_hide_name) ? 'none' : 'block');
-            document.documentElement.style.setProperty('--single-line-chat-badge', common.value(data.hide_badge, common.default_hide_badge) ? 'none' : 'block');
-            document.documentElement.style.setProperty('--single-line-chat-color', common.value(data.chat_color, common.default_chat_color) ? 'var(--yt-spec-text-secondary)' : 'var(--yt-live-chat-primary-text-color,var(--yt-spec-text-primary))');
-            document.documentElement.style.setProperty('--single-line-chat-color-sponsor', common.value(data.chat_color, common.default_chat_color) ? 'var(--yt-live-chat-sponsor-color)' : 'var(--yt-live-chat-primary-text-color,var(--yt-spec-text-primary))');
-            document.documentElement.style.setProperty('--single-line-chat-color-moderator', common.value(data.chat_color, common.default_chat_color) ? 'var(--yt-live-chat-moderator-color)' : 'var(--yt-live-chat-primary-text-color,var(--yt-spec-text-primary))');
-            document.documentElement.style.setProperty('--single-line-chat-bgcolor', common.value(data.chat_bgcolor, common.default_chat_bgcolor) ? 'var(--yt-spec-badge-chip-background)' : 'unset');
+            document.documentElement.style.setProperty('--single-line-chat-icon', common.value(data.hide_icon, common.default_hide_icon) ? 'none' : 'unset');
+            document.documentElement.style.setProperty('--single-line-chat-name', common.value(data.hide_name, common.default_hide_name) ? 'none' : 'unset');
+            document.documentElement.style.setProperty('--single-line-chat-badge', common.value(data.hide_badge, common.default_hide_badge) ? 'none' : 'unset');
+            document.documentElement.style.setProperty('--single-line-chat-color', common.value(data.chat_color, common.default_chat_color) ? 'var(--yt-live-chat-secondary-text-color)' : 'unset');
+            document.documentElement.style.setProperty('--single-line-chat-color-sponsor', common.value(data.chat_color, common.default_chat_color) ? 'var(--yt-live-chat-sponsor-color)' : 'unset');
+            document.documentElement.style.setProperty('--single-line-chat-color-moderator', common.value(data.chat_color, common.default_chat_color) ? 'var(--yt-live-chat-moderator-color)' : 'unset');
+            document.documentElement.style.setProperty('--single-line-chat-bgcolor', common.value(data.chat_bgcolor, common.default_chat_bgcolor) ? 'var(--yt-live-chat-secondary-background-color)' : 'unset');
         });
     }
 
